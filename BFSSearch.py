56b9dd1670edd6c4d930fbbcdc175c46bef4eddf
Mohamed Raslan <raslanmohamed.mr@gmail.com>
2021-11-12 20:20:26 +0200

    nvm

--- BFSSearch.py
+++ BFSSearch.py
@@ -3,13 +3,10 @@ import Node
 
 def solution(state, parent_map):
     sol = []
-    cost_to_goal = 0
     while parent_map[state]:
         sol.append(state)
         state = parent_map[state]
-        cost_to_goal += 1
     sol.reverse()
-    sol.append(cost_to_goal)
     return sol
 
 
@@ -18,18 +15,18 @@ def BFS(current_state, goal_state):
     frontier.add(current_state)
     explored = set()
     parent_map = {current_state: 0}
-    steps=0
+    steps = 0
     while frontier:
         state = frontier.pop()
         explored.add(state)
-        steps+=1
+        steps += 1
         if goal_state == state:
-            sol=solution(state,parent_map)
-            return state,len(sol)
+            sol = solution(state, parent_map)
+            return(sol, len(sol), steps)
         else:
             neighbours = Node.my_func(state)
             for neighbour in neighbours:
                 if (neighbour not in frontier) and (neighbour not in explored):
                     frontier.add(neighbour)
                     parent_map[neighbour] = state
-    return "lol"
+    return "lol", "lol"
--- DFSSearch.py
+++ DFSSearch.py
@@ -1,20 +1,34 @@
 import Node
 
 
+def solution(state, parent_map):
+    sol = []
+    cost_to_goal = 0
+    while parent_map[state]:
+        sol.append(state)
+        state = parent_map[state]
+        cost_to_goal += 1
+    sol.reverse()
+    sol.append(cost_to_goal)
+    return sol
+
+
 def dfs(initialState, goalState):
     stack = set()
     explored = set()
     stack.add(initialState)
+    parent_map = {initialState: 0}
     steps = 0
     while stack:
         currentstate = stack.pop()
         explored.add(currentstate)
         steps += 1
         if currentstate == goalState:
-            print(currentstate)
-            return (currentstate, steps)
+            sol=solution(currentstate, parent_map)
+            return currentstate, len(sol) - 1
         possiblestates = Node.my_func(currentstate)
         for states in possiblestates:
             if (states not in stack) and (states not in explored):
                 stack.add(states)
+                parent_map[states] = currentstate
     return "not found", steps
